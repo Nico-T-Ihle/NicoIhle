725226339a063279e8dc380da50ee97d4ea091d7
Nico Ihle <n.ihle2003@gmail.com>
2023-03-11 19:39:27 +0100

    NEW: Timeline

--- src/Components/main.js
+++ src/Components/main.js
@@ -9,6 +9,10 @@ import JvJ from '../assets/images/AppIcon.png'
 import Footer from './Footer';
 import Banner from './Banner';
 import Projects from './Projects';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 function Main() {
 
@@ -101,6 +105,49 @@ function Main() {
             <div className="cursor"></div>
 
             <Banner />
+
+            <div class="timeline">
+
+
+                <div data-aos="fade-left" class="containerTM left">
+                    <div class="content">
+                        <h2>2018</h2>
+                        <p>I took my first step in programming. At that point, and I had no clue what i was doing, but I found it fascinating, and that's how it all started.</p>
+                    </div>
+                </div>
+
+
+                <div data-aos="fade-right" class="containerTM right">
+                    <div class="content">
+                        <h2>2019</h2>
+                        <p>I started to build 2D pixel games with the game engine Unity. It was fun but also tough.</p>
+                    </div>
+                </div>
+
+
+                <div data-aos="fade-left" class="containerTM left">
+                    <div class="content">
+                        <h2>2020</h2>
+                        <p>I started my vocational training as a Fachinformatiker/in Anwendungsentwicklung. I learned standard web development and the basics of HTML, CSS, PHP, and a bit of WordPress.</p>
+                    </div>
+                </div>
+
+                <div data-aos="fade-right" class="containerTM right">
+                    <div class="content">
+                        <h2>2021</h2>
+                        <p>I fell in love with my first JavaScript framework called React. I had no clue what I was doing, but I tried my best to understand how things work. And also i started to Build my first Mobile App with React-Native it was so much fun.</p>
+                    </div>
+                </div>
+
+                <div data-aos="fade-left" class="containerTM left">
+                    <div class="content">
+                        <h2>2022</h2>
+                        <p>I've started to learn Swift/UI because I've always been interested in mobile development, so I started in my free time. I built some projects for iPadOS and iOS, and it was fun. I fell in love with programming in Swift.</p>
+                    </div>
+                </div>
+            </div>
+
+
             <Projects />
             <Footer />
         </>
